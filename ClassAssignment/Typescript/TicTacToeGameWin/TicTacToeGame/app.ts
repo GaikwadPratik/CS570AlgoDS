628f503eb66daf8c5a41d5f626589f0cb929ffdb
pratik <pratik.gaikwad19@gmail.com>
2016-09-20 21:06:29 -0400

    Removed same extra condition from other location

--- ClassAssignment/Typescript/TicTacToeGameWin/TicTacToeGame/app.ts
+++ ClassAssignment/Typescript/TicTacToeGameWin/TicTacToeGame/app.ts
@@ -107,7 +107,6 @@ class Program {
                 _bProceedWithGame = false;
         }//If user types load
         else if (_strGameStatus !== ''
-            && _strGameStatus.toUpperCase() !== UserInputProcessor.quit.toUpperCase()
             && _strGameStatus.toUpperCase() === UserInputProcessor.load.toUpperCase()) {
             //load from the file
             let _filePath: string = this._userInputProcessorObject.ShowQuestionToUser(UserInputProcessor.loadPath);
