c3980cc62110a946c5cf3b60c68c72e313c240da
orion <pratik.gaikwad19@gmail.com>
2016-10-30 00:38:53 -0400

    added explaination in the comment for this and _selfProgramObject

--- ClassAssignment/Typescript/HuffmanCoding/HuffmanCoding/app.js
+++ ClassAssignment/Typescript/HuffmanCoding/HuffmanCoding/app.js
@@ -1,90 +1,73 @@
-﻿import * as fs from 'fs';
-import * as path from 'path';
-import * as readline from 'readline';
-
+"use strict";
+const fs = require('fs');
+const path = require('path');
 class HuffmanNodeObject {
-    private _character: string = '';
-    private _frequency: number = 0;
-    private _binaryCode: string = '';
-    private _totalBits: number = 0;
-    private _leftNode: HuffmanNodeObject = null;
-    private _rightNode: HuffmanNodeObject = null;
-
-    public getCharacter(): string {
+    constructor() {
+        this._character = '';
+        this._frequency = 0;
+        this._binaryCode = '';
+        this._totalBits = 0;
+        this._leftNode = null;
+        this._rightNode = null;
+    }
+    getCharacter() {
         return this._character;
     }
-
-    public setCharacter(value: string): void {
+    setCharacter(value) {
         this._character = value;
     }
-
-    public getFrequency(): number {
+    getFrequency() {
         return this._frequency;
     }
-
-    public setFrequency(value: number): void {
+    setFrequency(value) {
         this._frequency = value;
     }
-
-    public getBinaryCode(): string {
+    getBinaryCode() {
         return this._binaryCode;
     }
-
-    public setBinaryCode(value: string): void {
+    setBinaryCode(value) {
         this._binaryCode = value;
     }
-
-    public getTotalBits(): number {
+    getTotalBits() {
         return this._totalBits;
     }
-
-    public setTotaBits(value: number): void {
+    setTotaBits(value) {
         this._totalBits = value;
     }
-
-    public getLeftNode(): HuffmanNodeObject {
+    getLeftNode() {
         return this._leftNode;
     }
-
-    public setLeftNode(value: HuffmanNodeObject): void {
+    setLeftNode(value) {
         this._leftNode = value;
     }
-
-    public getRightNode(): HuffmanNodeObject {
+    getRightNode() {
         return this._rightNode;
     }
-
-    public setRightNode(value: HuffmanNodeObject): void {
+    setRightNode(value) {
         this._rightNode = value;
     }
 }
-
 class Program {
-
-    private _lstHuffmanNodeObjects: Array<HuffmanNodeObject> = null;
-    private _lstHuffmanNodeTree: Array<HuffmanNodeObject> = null;
-
-    private _nodeRootHuffmanObject: HuffmanNodeObject = null;
-
-    private _totalCharacterCount = 0;
-    private _totalCodeLength = 0;
-    //name of the file.
-    private _inputFileName: string = "infile.dat";
-    //Reg ex to allow only numbers and characters.
-    private _regularExpression: RegExp = /[a-zA-Z0-9]/;
-
-    public Main(): void {
+    constructor() {
+        this._lstHuffmanNodeObjects = null;
+        this._lstHuffmanNodeTree = null;
+        this._nodeRootHuffmanObject = null;
+        this._totalCharacterCount = 0;
+        this._totalCodeLength = 0;
+        //name of the file.
+        this._inputFileName = "infile.dat";
+        //Reg ex to allow only numbers and characters.
+        this._regularExpression = /[a-zA-Z0-9]/;
+    }
+    Main() {
         try {
-
             //A copy of program object for reference in call backs.
-            let _selfProgramObject: Program = this;
-
+            let _selfProgramObject = this;
             //compelte file path till the folder
-            let _filePath: string = path.join(__dirname, this._inputFileName);
-
+            let _filePath = path.join(__dirname, this._inputFileName);
             //As per node documentation, instead of checking the existance first, we should directly perform operation and then handle the error within.
             //Open the file directly.
-            fs.open(_filePath, fs.R_OK, (_err: NodeJS.ErrnoException, _fd: number) => {
+            fs.open(_filePath, fs.R_OK, (_err, _fd) => {
                 //in case of an error, handle it
                 if (_err) {
                     if (_err.code === 'ENOENT')
@@ -92,50 +75,42 @@ class Program {
                     else
                         console.error(_err);
                 }
-                //else process data with in the file.
                 else {
                     //instead of reading whole file, we will read it letter by letter.
                     //create the stream to read the input file.
-                    let _readable: fs.ReadStream = fs.createReadStream(_filePath, {
+                    let _readable = fs.createReadStream(_filePath, {
                         encoding: 'utf8',
                         fd: null,
                     }).on('error', function (error) {
                         throw error;
                     });
-
                     //https://nodejs.org/api/stream.html#stream_readable_read_size
                     _readable.on('readable', () => {
                         try {
-                            let _chunk: string = '';
+                            let _chunk = '';
                             while (null !== (_chunk = _readable.read(1))) {
-                                this.ProcessCharacter(_chunk);//can use this here because it's inside arrow function.
+                                this.ProcessCharacter(_chunk); //can use this here because it's inside arrow function.
                             }
                         }
                         catch (exception) {
                             console.log(`An error occurred while reading data from file ${exception}`);
                         }
                     });
-
                     //once the file read operation is complete process start building graph.
                     _readable.on('end', function () {
                         if (this._totalCharacterCount === 0)
                             console.log(`${this._inputFileName} has no data. Please enter some data`);
                         else {
                             console.log(`number of letters in the file are ${this._totalCharacterCount}`);
-
                             //Sort the array by the frequency
                             _selfProgramObject._lstHuffmanNodeObjects = _selfProgramObject.SortCollectionInDescending(_selfProgramObject._lstHuffmanNodeObjects);
-
                             //Display the Character with their frequency
-                            _selfProgramObject.DisplayTable();//have to use _selfProgramObject since it's inside anonymous function of event.
-
+                            _selfProgramObject.DisplayTable(); //have to use _selfProgramObject since it's inside anonymous function of event.
                             //generation binary tree in the form of node list
                             _selfProgramObject._lstHuffmanNodeTree = _selfProgramObject._lstHuffmanNodeObjects;
                             _selfProgramObject._nodeRootHuffmanObject = _selfProgramObject.GenerateHuffMannTree();
-
                             //Generate binary codes of each letter
                             _selfProgramObject.CreateEncodings(_selfProgramObject._nodeRootHuffmanObject, '');
-
                             //Display the Character with their code
                             _selfProgramObject._lstHuffmanNodeTree = _selfProgramObject.SortCollectionInDescending(_selfProgramObject._lstHuffmanNodeTree);
                             _selfProgramObject.DisplayBinaryCode();
@@ -148,8 +123,7 @@ class Program {
             console.error(exception);
         }
     }
-
-    private CreateEncodings(rootObject: HuffmanNodeObject, binaryCode: string): void {
+    CreateEncodings(rootObject, binaryCode) {
         if (rootObject.getLeftNode() !== null) {
             this.CreateEncodings(rootObject.getLeftNode(), binaryCode + '0');
             this.CreateEncodings(rootObject.getRightNode(), binaryCode + '1');
@@ -159,9 +133,8 @@ class Program {
             this._lstHuffmanNodeTree.push(rootObject);
         }
     }
-
-    private SortCollectionInDescending(huffMannNodeCollection: Array<HuffmanNodeObject>): Array<HuffmanNodeObject> {
-        let _rtnVal: Array<HuffmanNodeObject> = null;
+    SortCollectionInDescending(huffMannNodeCollection) {
+        let _rtnVal = null;
         try {
             _rtnVal = huffMannNodeCollection.sort(function (firstNode, secondNode) {
                 try {
@@ -182,16 +155,15 @@ class Program {
         }
         return _rtnVal;
     }
-
-    private GenerateHuffMannTree(): HuffmanNodeObject {
-        let _rtnVal: HuffmanNodeObject = null;
-        let _leastFrequencyObject: HuffmanNodeObject = null;
-        let _secondLeastFrequencyObject: HuffmanNodeObject = null;
+    GenerateHuffMannTree() {
+        let _rtnVal = null;
+        let _leastFrequencyObject = null;
+        let _secondLeastFrequencyObject = null;
         while (this._lstHuffmanNodeTree.length > 1) {
             try {
                 _leastFrequencyObject = this._lstHuffmanNodeTree.pop();
                 _secondLeastFrequencyObject = this._lstHuffmanNodeTree.pop();
-                let _newCombinedHuffMannObject: HuffmanNodeObject = new HuffmanNodeObject();
+                let _newCombinedHuffMannObject = new HuffmanNodeObject();
                 _newCombinedHuffMannObject.setLeftNode(_leastFrequencyObject);
                 _newCombinedHuffMannObject.setRightNode(_secondLeastFrequencyObject);
                 _newCombinedHuffMannObject.setFrequency(_leastFrequencyObject.getFrequency() + _secondLeastFrequencyObject.getFrequency());
@@ -205,34 +177,30 @@ class Program {
         _rtnVal = this._lstHuffmanNodeTree.pop();
         return _rtnVal;
     }
-
-    private DisplayTable(): void {
+    DisplayTable() {
         console.log('symbol\tfrequency');
         for (let _tempHuffmannObject of this._lstHuffmanNodeObjects) {
-            let _fre: number = _tempHuffmannObject.getFrequency();
+            let _fre = _tempHuffmannObject.getFrequency();
             _fre = ((_fre / this._totalCharacterCount) * 100);
             console.log(`${_tempHuffmannObject.getCharacter()},\t${Number(Math.round(+(_fre + 'e2')) + 'e-2').toFixed(2)}%`);
         }
     }
-
-    private DisplayBinaryCode(): void {
+    DisplayBinaryCode() {
         for (let _tempHuffmannObject of this._lstHuffmanNodeTree) {
             console.log(`${_tempHuffmannObject.getCharacter()},\t${_tempHuffmannObject.getFrequency()},\t${_tempHuffmannObject.getBinaryCode()} `);
             this._totalCodeLength += this._totalCodeLength + (_tempHuffmannObject.getFrequency() * _tempHuffmannObject.getBinaryCode().length);
         }
-
         console.log(`Total length of the message is ${this._totalCodeLength} bits`);
     }
-
-    private ProcessCharacter(chunkData: string): void {
+    ProcessCharacter(chunkData) {
         if (typeof (chunkData) !== 'undefined'
             && chunkData !== null
             && this._regularExpression.test(chunkData)) {
             //console.log(_chunk); // chunk is one symbol
             if (typeof (this._lstHuffmanNodeObjects) === 'undefined' || this._lstHuffmanNodeObjects === null)
-                this._lstHuffmanNodeObjects = new Array<HuffmanNodeObject>();
+                this._lstHuffmanNodeObjects = new Array();
             //look up if the character is already present in the array.
-            let _existingObject: HuffmanNodeObject = this._lstHuffmanNodeObjects.find(x => x.getCharacter() === chunkData);
+            let _existingObject = this._lstHuffmanNodeObjects.find(x => x.getCharacter() === chunkData);
             if (typeof (_existingObject) === 'undefined' || _existingObject === null) {
                 //if not then add it up
                 _existingObject = new HuffmanNodeObject();
@@ -242,9 +210,9 @@ class Program {
             }
             else {
                 //if present increment the frequency 
-                let _position: number = this._lstHuffmanNodeObjects.findIndex(x => x === _existingObject);
+                let _position = this._lstHuffmanNodeObjects.findIndex(x => x === _existingObject);
                 //get the original frequency
-                let _originalFrequency: number = _existingObject.getFrequency();
+                let _originalFrequency = _existingObject.getFrequency();
                 //Increment the frequency
                 _existingObject.setFrequency(++_originalFrequency);
                 //Update the object in collection
@@ -254,5 +222,5 @@ class Program {
         }
     }
 }
-
 new Program().Main();
+//# sourceMappingURL=app.js.map
