779a5ff59e7b213b639fe67618e5361ef72d313e
orion <pratik.gaikwad19@gmail.com>
2016-11-04 14:04:15 -0400

    with correct implementation of properties in Typescript

--- Labwork/Typescript/SortedBinarySearchTree/SortedBinarySearchTree/app.js
+++ Labwork/Typescript/SortedBinarySearchTree/SortedBinarySearchTree/app.js
@@ -1,54 +1,61 @@
-﻿import * as fs from 'fs';
-import * as path from 'path';
-let readlineSync = require('readline-sync');
-
-class NodeObject<T>{
-    private _data: T = null;
-    private _nodeLeft: NodeObject<T> = null;
-    private _nodeRight: NodeObject<T> = null;
-
-    public get Data(): T {
-        return this._data;
+"use strict";
+var fs = require('fs');
+var path = require('path');
+var readlineSync = require('readline-sync');
+var NodeObject = (function () {
+    function NodeObject() {
+        this._data = null;
+        this._nodeLeft = null;
+        this._nodeRight = null;
     }
-    public set Data(value: T) {
-        this._data = value;
+    Object.defineProperty(NodeObject.prototype, "Data", {
+        get: function () {
+            return this._data;
+        },
+        set: function (value) {
+            this._data = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(NodeObject.prototype, "LeftNode", {
+        get: function () {
+            return this._nodeLeft;
+        },
+        set: function (nodeValue) {
+            this._nodeLeft = nodeValue;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(NodeObject.prototype, "RightNode", {
+        get: function () {
+            return this._nodeRight;
+        },
+        set: function (nodeValue) {
+            this._nodeRight = nodeValue;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return NodeObject;
+}());
+var SortedSet = (function () {
+    function SortedSet() {
+        this._rootNodeObject = null;
     }
-
-    public get LeftNode(): NodeObject<T> {
-        return this._nodeLeft;
-    }
-
-    public set LeftNode(nodeValue: NodeObject<T>) {
-        this._nodeLeft = nodeValue;
-    }
-
-    public get RightNode(): NodeObject<T> {
-        return this._nodeRight;
-    }
-
-    public set RightNode(nodeValue: NodeObject<T>) {
-        this._nodeRight = nodeValue;
-    }
-}
-
-class SortedSet<T> {
-    private _rootNodeObject: NodeObject<T> = null;
-
-    public getRootNode(): NodeObject<T> {
+    SortedSet.prototype.getRootNode = function () {
         return this._rootNodeObject;
-    }
-
-    public add(value: T): void {
+    };
+    SortedSet.prototype.add = function (value) {
         try {
-            let _newNode: NodeObject<T> = new NodeObject<T>();
+            var _newNode = new NodeObject();
             _newNode.Data = value;
-
             if (this._rootNodeObject === null)
                 this._rootNodeObject = _newNode;
             else {
-                let _currentNode: NodeObject<T> = this._rootNodeObject;
-                let _parentNode: NodeObject<T> = null;
-
+                var _currentNode = this._rootNodeObject;
+                var _parentNode = null;
                 while (true) {
                     _parentNode = _currentNode;
                     if (_currentNode.Data > value) {
@@ -69,25 +76,23 @@ class SortedSet<T> {
             }
         }
         catch (exception) {
-            console.error(`error occurred in 'Add'. Error info: ${exception}`);
+            console.error("error occurred in 'Add'. Error info: " + exception);
         }
-    }
-
-    public IsEmpty(): boolean {
-        let _bRtnVal = false;
+    };
+    SortedSet.prototype.IsEmpty = function () {
+        var _bRtnVal = false;
         try {
             _bRtnVal = this._rootNodeObject === null;
         }
         catch (exception) {
-            console.error(`error occurred in 'IsEmpty'. Error info: ${exception}`);
+            console.error("error occurred in 'IsEmpty'. Error info: " + exception);
         }
         return _bRtnVal;
-    }
-
-    public Contains(value: T): boolean {
-        let _bRtnVal = false;
+    };
+    SortedSet.prototype.Contains = function (value) {
+        var _bRtnVal = false;
         try {
-            let _current: NodeObject<T> = this._rootNodeObject;
+            var _current = this._rootNodeObject;
             while (_current !== null) {
                 if (_current.Data === value) {
                     _bRtnVal = true;
@@ -100,26 +105,20 @@ class SortedSet<T> {
             }
         }
         catch (exception) {
-            console.error(`error occurred in 'Contains'. Error info: ${exception}`);
+            console.error("error occurred in 'Contains'. Error info: " + exception);
         }
         return _bRtnVal;
-    }
-
-    public Remove(value: T): boolean {
-        let _bRtnval = false;
+    };
+    SortedSet.prototype.Remove = function (value) {
+        var _bRtnval = false;
         try {
-
             if (!this.IsEmpty()) {
-                let _parentNode: NodeObject<T> = this._rootNodeObject;
-                let _currentNode: NodeObject<T> = this._rootNodeObject;
-
-                let _bValueFound: boolean = true;
-                let _bInLeftNode: boolean = false;
-
-
+                var _parentNode = this._rootNodeObject;
+                var _currentNode = this._rootNodeObject;
+                var _bValueFound = true;
+                var _bInLeftNode = false;
                 while (_currentNode.Data !== value) {
                     _parentNode = _currentNode;
-
                     if (_currentNode === null) {
                         _bValueFound = false;
                         break;
@@ -133,8 +132,6 @@ class SortedSet<T> {
                         _currentNode = _currentNode.RightNode;
                     }
                 }
-
-
                 if (_bValueFound) {
                     if (_currentNode.LeftNode === null && _currentNode.RightNode === null) {
                         if (_currentNode === this._rootNodeObject)
@@ -161,14 +158,13 @@ class SortedSet<T> {
                             _parentNode.RightNode = _currentNode.RightNode;
                     }
                     else if (_currentNode.LeftNode !== null && _currentNode.RightNode !== null) {
-                        let _nextNode: NodeObject<T> = this.GetNextNode(_currentNode);
+                        var _nextNode = this.GetNextNode(_currentNode);
                         if (_currentNode === this._rootNodeObject)
                             this._rootNodeObject = _nextNode;
                         else if (_bInLeftNode)
                             _parentNode.LeftNode = _nextNode;
                         else
                             _parentNode.RightNode = _nextNode;
-
                         _nextNode.LeftNode = _currentNode.LeftNode;
                     }
                     _bRtnval = true;
@@ -180,103 +176,100 @@ class SortedSet<T> {
                 _bRtnval = false;
         }
         catch (exception) {
-            console.error(`error occurred in 'Delete'. Error info: ${exception}`);
+            console.error("error occurred in 'Delete'. Error info: " + exception);
         }
         return _bRtnval;
-    }
-
-    private GetNextNode(currentNode: NodeObject<T>): NodeObject<T> {
-        let _rtnNode: NodeObject<T> = null;
+    };
+    SortedSet.prototype.GetNextNode = function (currentNode) {
+        var _rtnNode = null;
         try {
-            let _parentNode: NodeObject<T> = null;
-            let _currentNode: NodeObject<T> = currentNode.RightNode;
+            var _parentNode = null;
+            var _currentNode = currentNode.RightNode;
             while (_currentNode !== null) {
                 _parentNode = _rtnNode;
                 _rtnNode = _currentNode;
                 _currentNode = _currentNode.LeftNode;
             }
-
             if (_rtnNode !== currentNode.RightNode) {
                 _parentNode.LeftNode = _rtnNode.RightNode;
                 _rtnNode.RightNode = currentNode.RightNode;
             }
         }
         catch (exception) {
-            console.error(`error occurred in 'GetNextNode'. Error info: ${exception}`);
+            console.error("error occurred in 'GetNextNode'. Error info: " + exception);
         }
         return _rtnNode;
-    }
-
-    public Display(rootNode: NodeObject<T>): void {
+    };
+    SortedSet.prototype.Display = function (rootNode) {
         if (rootNode !== null) {
             this.Display(rootNode.LeftNode);
-            process.stdout.write(`${rootNode.Data}`);
+            process.stdout.write("" + rootNode.Data);
             if (rootNode.RightNode !== null)
                 process.stdout.write(',');
             process.stdout.write(' ');
             this.Display(rootNode.RightNode);
         }
+    };
+    return SortedSet;
+}());
+var Program = (function () {
+    function Program() {
+        this._regularExpression = /[0-9]/;
+        this._inputFileName = "infile.dat";
+        this._sortedSet = null;
     }
-}
-
-class Program {
-    private _regularExpression: RegExp = /[0-9]/;
-    private _inputFileName: string = "infile.dat";
-    private _sortedSet: SortedSet<number> = null;
-    public Main(): void {
-        let _selfProgramObject: Program = this;
-
-        let _filePath: string = path.join(__dirname, this._inputFileName);
-        fs.open(_filePath, fs.R_OK, (_err: NodeJS.ErrnoException, _fd: number) => {
+    Program.prototype.Main = function () {
+        var _this = this;
+        var _selfProgramObject = this;
+        var _filePath = path.join(__dirname, this._inputFileName);
+        fs.open(_filePath, fs.R_OK, function (_err, _fd) {
             //in case of an error, handle it
             if (_err) {
                 if (_err.code === 'ENOENT')
-                    console.error(`The file with name '${this._inputFileName}' is not found in directory '${__dirname}'. Please check if filename is correct or not.`);
+                    console.error("The file with name '" + _this._inputFileName + "' is not found in directory '" + __dirname + "'. Please check if filename is correct or not.");
                 else
                     console.error(_err);
             }
             else {
-                let _readable: fs.ReadStream = fs.createReadStream(_filePath, {
+                var _readable_1 = fs.createReadStream(_filePath, {
                     encoding: 'utf8',
                     fd: null,
                 }).on('error', function (error) {
                     throw error;
                 });
-
                 //https://nodejs.org/api/stream.html#stream_readable_read_size
-                _readable.on('readable', function () {
+                _readable_1.on('readable', function () {
                     try {
-                        let _chunk: string = '';
-                        while (null !== (_chunk = _readable.read(1))) {
+                        var _chunk = '';
+                        while (null !== (_chunk = _readable_1.read(1))) {
                             if (_selfProgramObject._regularExpression.test(_chunk)) {
                                 //Add inside the binary tree
                                 if (_selfProgramObject._sortedSet === null)
-                                    _selfProgramObject._sortedSet = new SortedSet<number>();
+                                    _selfProgramObject._sortedSet = new SortedSet();
                                 _selfProgramObject._sortedSet.add(+_chunk);
                             }
                             else if (_chunk !== ',')
-                                console.log(`An invalid character '${_chunk}' was found in ${_filePath}. Please make sure it contains number only.`);
+                                console.log("An invalid character '" + _chunk + "' was found in " + _filePath + ". Please make sure it contains number only.");
                         }
                     }
                     catch (exception) {
-                        console.log(`An error occurred while reading data from file ${exception}`);
+                        console.log("An error occurred while reading data from file " + exception);
                     }
                 });
-
                 //once the file read operation is complete process start building graph.
-                _readable.on('end', () => {
-                    process.stdout.write(`Sorted Set A Contains `);
-                    this._sortedSet.Display(this._sortedSet.getRootNode());
+                _readable_1.on('end', function () {
+                    process.stdout.write("Sorted Set A Contains\u00A0");
+                    _this._sortedSet.Display(_this._sortedSet.getRootNode());
                     console.log();
-                    let _userResponse: string = readlineSync.question('', { limit: [/^\d+$/], limitMessage: "Enter numeric value only" });
-
-                    console.log(`User Input = ${+_userResponse}`);
-                    console.log(`Output`);
-                    console.log(this._sortedSet.Contains(+_userResponse));
+                    var _userResponse = readlineSync.question('', { limit: [/^\d+$/], limitMessage: "Enter numeric value only" });
+                    console.log("User Input = " + +_userResponse);
+                    console.log("Output");
+                    console.log(_this._sortedSet.Contains(+_userResponse));
                 });
             }
         });
-    }
-}
-
+    };
+    return Program;
+}());
 new Program().Main();
+//# sourceMappingURL=app.js.map
